2af53e04c2c702c7107b88016ed60a0332fc310e
Cheng-H-Y <alex.chenghaoyang@gmail.com>
2015-07-14 23:46:52 +0800

    completed

--- Memory.py
+++ Memory.py
@@ -4,10 +4,17 @@ import simplegui
 import random
 card=[]
 expose=[]
-state=0
+state= 0
+pair_values = []
+pair_index = []
+turn=0
 # helper function to initialize globals
 def new_game():
-
+    global turn,card,expose
+    card=[]
+    expose=[]
+    state= 0
+    turn = 0
     for i in range(8):
         card.append(i)
         card.append(i)
@@ -20,11 +27,28 @@ def new_game():
 # define event handlers
 def mouseclick(pos):
     # add game state logic here
-    global state
+    global state,pair_index,pair_values,turn
     for i in range(16):
         if pos[0] < (i+1)*50:break
-    expose[i]=True
-    pass
+    if expose[i] == False:
+        label.set_text("Turns = %d " %turn)
+        expose[i]= True
+        pair_index.append(i)
+        pair_values.append(card[i])
+        if state == 1:state = 2
+        elif state == 2:
+            if pair_values[1] != pair_values[0]:
+                expose[pair_index[0]]=False
+                expose[pair_index[1]]=False		
+            else:pass     
+            pair_values.pop(0)
+            pair_values.pop(0)
+            pair_index.pop(0)
+            pair_index.pop(0)
+            state =1
+            turn += 1
+        else:state = 1		
+    else:pass
 
 
 # cards are logically 50x100 pixels in size
@@ -43,7 +67,7 @@ def draw(canvas):
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
 frame.add_button("Reset", new_game)
-label = frame.add_label("Turns = 0")
+label = frame.add_label("Turns = %d " %turn)
 
 # register event handlers
 frame.set_mouseclick_handler(mouseclick)
@@ -54,4 +78,4 @@ new_game()
 frame.start()
 
 
-# Always remember to review the grading rubric
+
