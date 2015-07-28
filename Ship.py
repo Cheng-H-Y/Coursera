1d2657e683161774f41269070c745143bc59aa22
Cheng-H-Y <alex.chenghaoyang@gmail.com>
2015-07-28 21:20:31 +0800

    delete

--- Ship.py
+++
@@ -1,1 +0,0 @@
-# template for "Ship"# run on http://www.codeskulptor.org/class ship    def __init__(self,pos,vel,angle,image,info):        self.pos=pos[]        self.vel=vel[]        self.angle=angle        self.thrust=False        self.angle_vel=0        self.image=image    def draw(self,canvas):        canvas.
